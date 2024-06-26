0b9795623de84651ab1a9ba8360ab7143797b822
kmhk-naka <phytran35@gmail.com>
2024-06-26 23:41:39 +0900

    add mouse drag support

--- src/components/DrawingCanvas.vue
+++ src/components/DrawingCanvas.vue
@@ -3,15 +3,17 @@
     <AsciiCharacterPane class="character-pane" @select-char="onSelectActiveChar" />
 
     <div class="canvas-container">
-      <table id="canvas">
+      <table id="canvas" @mousedown="startDrag" @mouseup="stopDrag" @mouseleave="stopDrag">
         <tbody>
           <tr v-for="(row, rowIndex) in canvas" :key="`row-${rowIndex}`" class="row">
             <td
               v-for="(cell, colIndex) in row"
               :key="`col-${colIndex}`"
+              :style="cellStyle"
               class="cell"
-              @click.prevent="fillActiveChar(rowIndex, colIndex)"
+              @click.prevent="onClickCell(rowIndex, colIndex)"
               @contextmenu.prevent="fillDefaultChar(rowIndex, colIndex)"
+              @mousemove="onMousemoveCell($event, rowIndex, colIndex)"
             >
               {{ cell }}
             </td>
@@ -30,20 +32,32 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onUnmounted } from 'vue';
+import { computed, ref, onUnmounted } from 'vue';
 
 import AsciiCharacterPane from './AsciiCharacterPane.vue';
 
 const CANVAS_SIZES = { rows: 24, cols: 60 };
 const DEFAULT_CHAR = ' ';
+const DEFAULT_FONT_SIZE = 16;
 
 const canvas = ref(
   Array.from(Array(CANVAS_SIZES.rows), () => new Array(CANVAS_SIZES.cols).fill(DEFAULT_CHAR)),
 );
 
+const fontSize = ref(DEFAULT_FONT_SIZE);
+const cellWidth = computed(() => fontSize.value);
+const cellHeight = computed(() => fontSize.value * 1.25);
+const cellStyle = computed(() => ({
+  '--cell-width': `${cellWidth.value}px`,
+  '--cell-height': `${cellHeight.value}px`,
+}));
+
 const activeChar = ref(DEFAULT_CHAR);
 
 const isVisibleCopyMsg = ref(false);
+
+const isDragging = ref(false);
+const draggingButton = ref<number | null>(null);
 
 const fillActiveChar = (row: number, col: number) => {
   canvas.value[row][col] = activeChar.value;
@@ -51,6 +65,47 @@ const fillActiveChar = (row: number, col: number) => {
 
 const fillDefaultChar = (row: number, col: number) => {
   canvas.value[row][col] = DEFAULT_CHAR;
+};
+
+const onClickCell = (row: number, col: number) => {
+  fillActiveChar(row, col);
+};
+
+const checkValidOffset = ({ offsetX, offsetY }: { offsetX: number; offsetY: number }) => {
+  const ignoreOffsetWidth = Math.min(cellWidth.value * 0.4, 8);
+  const ignoreOffsetHeight = Math.min(cellHeight.value * 0.4, 8);
+  return !(
+    (offsetY <= ignoreOffsetHeight && // Ignore upper left and right corner
+      (offsetX <= ignoreOffsetWidth || offsetX >= cellWidth.value - ignoreOffsetWidth)) ||
+    (offsetY >= cellHeight.value - ignoreOffsetHeight && // Ignore lower left and right corner
+      (offsetX <= ignoreOffsetWidth || offsetX >= cellWidth.value - ignoreOffsetWidth))
+  );
+};
+
+const onMousemoveCell = (e: MouseEvent, row: number, col: number) => {
+  if (!checkValidOffset(e)) return;
+
+  if (!isDragging.value) return;
+
+  switch (draggingButton.value) {
+    case 0:
+      return fillActiveChar(row, col);
+    case 1:
+      return fillDefaultChar(row, col);
+    default:
+      return;
+  }
+};
+
+const startDrag = (e: MouseEvent) => {
+  if ([0, 1].includes(e.button)) {
+    isDragging.value = true;
+    draggingButton.value = e.button;
+  }
+};
+
+const stopDrag = (e: MouseEvent) => {
+  isDragging.value = false;
 };
 
 const onSelectActiveChar = (char: string) => {
@@ -87,13 +142,14 @@ onUnmounted(() => {
 
   .canvas-container {
     #canvas {
-      $cell-width: 12px;
-      $cell-height: 16px;
+      $cell-width: var(--cell-width);
+      $cell-height: var(--cell-height);
 
       table-layout: fixed;
       width: calc(#{$cell-width} * 60);
       border-collapse: collapse;
       margin-top: 48px;
+      user-select: none;
 
       .cell {
         border: 1px solid lightgray;
@@ -101,7 +157,6 @@ onUnmounted(() => {
         height: $cell-height;
         padding: 0;
         text-align: center;
-        cursor: pointer;
         font-size: $cell-width;
         line-height: 1;
       }
